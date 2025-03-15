e7a83dc835090d3d3fa31ebbb1e3b13e21af2e08
Sailakshmi-0987 <gajulapallisailakshmi@gmail.com>
2025-03-15 16:46:16 +0530

    dynamic data to signup

--- src/app.js
+++ src/app.js
@@ -1,8 +1,27 @@
 const express = require("express");
+const db=require("./config/database");
 const app = express();
-app.use('/',(req,res)=>{
-    res.send("Hello people!");
-})
-app.listen(3000,()=>{
-    console.log("server is listening at the port 3000");
+const User = require("./models/user");
+app.use(express.json());
+
+app.post('/signup',async (req,res)=>{
+    const user = new User(req.body);
+    try{
+        await user.save();
+        res.send("user added succesfully");
+    }catch (err) {
+        res.status(400).send("Error adding the user:" + err.message);
+    }
 });
+
+
+db()
+    .then(()=>{
+        console.log("Database connection established");
+        app.listen(3000,()=>{
+            console.log("server is listening at the port 3000");
+        });
+    })
+    .catch((err)=>{
+        console.error("Database cannot be connected!!");
+    });
---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () =>  {
+    await mongoose.connect(
+        "mongodb+srv://sailakshmi:tom%407075@dev.ct3gr.mongodb.net/devTinder"
+    );
+};
+module.exports=connectDB;
+
+//const URL=
+   // "mongodb+srv://sailakshmi:tom@7075@dev.ct3gr.mongodb.net/?"
