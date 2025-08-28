81d1e9c4c26548d5db5a52d2e82c0aca0beda016
Sailakshmi-0987 <gajulapallisailakshmi@gmail.com>
2025-08-28 19:50:01 +0530

    updated app.js

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:process.env.FRONTEND_URL||"http://localhost:5173",
+    origin:["https://dev-tinder-frontend-tan.vercel.app","http://localhost:5173"],
     credentials:true,
 }));
 app.get("/", (req, res) => {
